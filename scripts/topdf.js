08ae41278448f1b636122f1acd58e3cd6cdc559b
Gerard Braad <me@gbraad.nl>
2016-09-22 06:16:23 +0000

    Allow papersize for PDF generation

--- scripts/topdf.js
+++ scripts/topdf.js
@@ -2,11 +2,14 @@
 var page = require('webpage').create(),
     system = require('system'),
     address = system.args[1],
-    output = system.args[2];
+    output = system.args[2],
+    paperSize;
 
 page.viewportSize = { width: 600, height: 600 };
-page.paperSize = { format: "A4", orientation: 'portrait', margin: '1cm' };
+paperSize = { format: "A4", orientation: 'portrait', margin: '1cm' };
 if(system.args.length > 3) { page.zoomFactor = system.args[3]; }
+if(system.args.length > 4) { paperSize.format = system.args[4]; }
+page.paperSize = paperSize;
 page.open(address, function (status) {
     if (status !== 'success') {
         console.log('Unable to load the address!');
